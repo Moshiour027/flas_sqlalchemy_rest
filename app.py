e077228dff7b815c2a1275a9ba4a5d7c30511f35
moshiour <moshiour0027@gmail.com>
2020-01-13 00:13:47 +0600

    update

--- app.py
+++ app.py
@@ -7,10 +7,43 @@ import os
 
 app = Flask(__name__)
 
+baseDir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/', methods=['GET'])
-def index():
-    return jsonify({'msg': 'Hello Techyowls!'})
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///' + os.path.join(baseDir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init ma
+ma = Marshmallow(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
+
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'qty')
+
+
+# Init Schema
+
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+
 
 
 # Run Server
